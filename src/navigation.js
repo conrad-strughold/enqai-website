7d6aa5fcf13906fc9e53a2821e9416acd3746510
coinlordd <coinlord.developer@gmail.com>
2023-12-17 21:18:12 +0100

    Update navigation.js

--- src/navigation.js
+++ src/navigation.js
@@ -12,5 +12,9 @@ export const headerData = {
       text: 'WHITEPAPER',
       href: 'https://enqai.com/wp',
     },
+    {
+      text: 'TRADE',
+      href: 'https://app.uniswap.org/swap?exactField=input&exactAmount=10&inputCurrency=0x710287D1D39DCf62094A83EBB3e736e79400068a&outputCurrency=ETH',
+    },
   ],
 }
